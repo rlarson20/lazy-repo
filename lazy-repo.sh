e77271743acf9ccae67667f2b2a2c6258d2e1105
rlarson20 <90066470+rlarson20@users.noreply.github.com>
2024-09-17 13:22:07 -0400

    moved gather-repo functionality into lazy-repo need to extend the lazy-plugins function to other plugin managers also added basic argument support and etc need to replace the $2s with $OPTARGs instead

--- lazy-repo.sh
+++ lazy-repo.sh
@@ -1,9 +1,45 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-	./gather-repos.sh | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
-	exit 0
-fi
+lazy_repo_gather() {
+	location_of_installed_plugins="$HOME/.local/share/nvim/lazy"
+	cd "$location_of_installed_plugins"
+	rg --only-matching --no-filename --max-count=1 "http.*" ./*/.git/config | sed 's/.git$//' | sed 's/.*com\///' | sort -i
+}
 
-cat $1 | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
-exit 0
+directory_repo_gather() {
+	repo_location=$2
+	cd "$repo_location"
+	rg --only-matching --no-filename ".*github\.com.*" | sort -iu | sed 's/.*com\///'
+}
+
+directory_search() {
+	directory_repo_gather "$@" | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
+}
+
+lazy_plugins() {
+	lazy_repo_gather | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
+}
+
+file_search() {
+	cat "$2" | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
+}
+while getopts ":dlf" option; do
+	case $option in
+	d) # directory search
+		directory_search "$@"
+		exit 0
+		;;
+	l) # lazy plugin search
+		lazy_plugins
+		exit 0
+		;;
+	f) # file repo search
+		file_search "$@"
+		exit 0
+		;;
+	?) #invalid args
+		echo "Invalid arguments, sorry."
+		;;
+	esac
+done
+echo "No arguments, please use -d for directory search, -l for lazy plugin search, -f for file search"
