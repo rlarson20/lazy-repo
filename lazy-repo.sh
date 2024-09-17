b58cebbc921429d37c1661df182a620bb65804bf
rlarson20 <90066470+rlarson20@users.noreply.github.com>
2024-09-17 12:36:26 -0400

    added default repo source and updated lazy-repo.sh

--- lazy-repo.sh
+++ lazy-repo.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+	./gather-repos.sh | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
+	exit 0
+fi
+
 cat $1 | fzf --prompt="lazy-repo> " --preview="gh repo view {}"
+exit 0
